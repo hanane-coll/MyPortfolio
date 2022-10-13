9c5349cf6da564a8f361a1662fa005441ba3bee4
Anna Jacob <hanane.yacoubi.5@gmail.com>
2022-10-13 16:06:21 +0100

    Updates

---
+++ precache-manifest.5e9911b474f14da88bf2ac860d3fb64d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0443d7a0fec6c63cc5f50e622f007f5f",
+    "url": "/MyPortfolio/index.html"
+  },
+  {
+    "revision": "1192e7edf4545ee2e7ad",
+    "url": "/MyPortfolio/static/css/2.bddd2539.chunk.css"
+  },
+  {
+    "revision": "f4bea11b86cd6d4f37de",
+    "url": "/MyPortfolio/static/css/main.50e6efab.chunk.css"
+  },
+  {
+    "revision": "1192e7edf4545ee2e7ad",
+    "url": "/MyPortfolio/static/js/2.f3c39678.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/MyPortfolio/static/js/2.f3c39678.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4bea11b86cd6d4f37de",
+    "url": "/MyPortfolio/static/js/main.04fe6b30.chunk.js"
+  },
+  {
+    "revision": "53bd5cae31acfb51d978",
+    "url": "/MyPortfolio/static/js/runtime-main.5f4b4c4c.js"
+  }
+]);
